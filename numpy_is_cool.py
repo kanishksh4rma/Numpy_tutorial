23ebd898295dccd4639c97b28d65ec04d523a741
lonewolf-X <61106875+lonewolf-X@users.noreply.github.com>
2020-02-25 12:22:40 +0530

    Add files via upload

---
+++ numpy_is_cool.py
@@ -0,0 +1,9 @@
+import numpy as np
+#a = np.array([1,2,3,4,5])
+b = np.zeros((5,6),int)	#creates array & initialize it with 0
+#b = np.ones(5,int)
+#b = np.full((5,6),56,int)
+#b = np.arange(0,11)
+#b = np.linspace(1,21,7)
+print(b)
+
