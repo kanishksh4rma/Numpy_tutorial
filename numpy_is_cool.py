a1a455fe372585f389e0dd1fc63e9f86c7199a08
lonewolf-X <61106875+lonewolf-X@users.noreply.github.com>
2020-02-26 10:59:20 +0530

    Update numpy_is_cool.py

--- numpy_is_cool.py
+++ numpy_is_cool.py
@@ -1,9 +1,9 @@
 import numpy as np
 #a = np.array([1,2,3,4,5])
-b = np.zeros((5,6),int)	#creates array & initialize it with 0
-#b = np.ones(5,int)
-#b = np.full((5,6),56,int)
-#b = np.arange(0,11)
-#b = np.linspace(1,21,7)
+b = np.zeros((5,6),int)	    #creates array & initialize it with 0
+#b = np.ones(5,int)         # it initializes an array of size 5 with 0's
+#b = np.full((5,6),56,int)  #it initializes 2-d array with value 56 as initial value
+#b = np.arange(0,11)        # initializes array from 0 to 10
+#b = np.linspace(1,21,7)    # array with 7 elements from 1 to 20 
 print(b)
 
